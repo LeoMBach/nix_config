24df5965b79258401522b162bc83943ead823f18
Leo M Bach <leo.gt.bach@gmail.com>
2018-09-28 12:21:06 +0200

    Add mintty themes

--- setup.sh
+++ setup.sh
@@ -4,3 +4,11 @@ dotfiles="$(find .* -maxdepth 0 -type f)"
 for dot in $dotfiles; do
     cp -vf $dot $HOME/$dot
 done;
+
+if [ ! -d "~/.mintty/themes" ]; then
+    mkdir -p ~/.mintty/themes
+fi
+themes="$(find ./mintty_themes/*)"
+for theme in $themes; do
+    cp -vf $theme ~/.mintty/themes 
+done;
