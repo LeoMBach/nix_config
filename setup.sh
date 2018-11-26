4c293239c997581989ae870905f33b7e1f752bbe
Leo M Bach <leo.gt.bach@gmail.com>
2018-11-26 15:30:46 +0100

    Remove quotes around paths

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ for dot in $dotfiles; do
     cp -vf $dot $HOME/$dot
 done;
 
-if [ ! -d "~/.mintty/themes" ]; then
+if [ ! -d ~/.mintty/themes ]; then
     mkdir -p ~/.mintty/themes
 fi
 themes="$(find ./mintty_themes/*)"
@@ -13,6 +13,6 @@ for theme in $themes; do
     cp -vf $theme ~/.mintty/themes 
 done;
 
-if [ ! -d "~/.bash-git-prompt" ]; then
+if [ ! -d ~/.bash-git-prompt ]; then
     git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 fi
