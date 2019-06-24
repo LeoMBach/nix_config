b20a9b7cef9157c0631f6e39034bea61d2f262eb
Leo M Bach <leo.gt.bach@gmail.com>
2019-06-24 14:27:17 +0200

    Add info messages and better spacing

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo ""
+
+echo "Copying dotfiles..."
 dotfiles="$(find ./dotfiles/.* -maxdepth 0 -type f -printf '%f\n')"
 for dot in $dotfiles; do
     cp -vf ./dotfiles/$dot $HOME/$dot
 done;
 
+echo ""
+
+echo "Installing mintty themes.."
 if [ ! -d ~/.mintty/themes ]; then
     mkdir -p ~/.mintty/themes
 fi
@@ -13,6 +19,15 @@ for theme in $themes; do
     cp -vf $theme ~/.mintty/themes 
 done;
 
+echo ""
+
+echo "Installing bash-git-prompt..."
 if [ ! -d ~/.bash-git-prompt ]; then
     git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+else
+    echo "bash-git-prompt is already installed: skipping!"
 fi
+
+echo ""
+
+echo "DONE!"
