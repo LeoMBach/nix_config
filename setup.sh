5b22bfdd0280b31c561d33cfb49abbc9dd75c95b
Leo M Bach <leo.gt.bach@gmail.com>
2019-12-13 08:46:20 +0100

    Remove mintty themes
    These will need to be migrated to winconfig

--- setup.sh
+++ setup.sh
@@ -7,16 +7,6 @@ function copy_dotfiles() {
     done;
 }
 
-function copy_mintty_themes() {
-    if [ ! -d ~/.mintty/themes ]; then
-        mkdir -p ~/.mintty/themes
-    fi
-    themes="$(find ./mintty_themes/*)"
-    for theme in $themes; do
-        cp -vf $theme ~/.mintty/themes 
-    done;
-}
-
 function install_bash_git_prompt() {
     if [ ! -d ~/.bash-git-prompt ]; then
         git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
@@ -29,10 +19,6 @@ echo ""
 
 echo "Copying dotfiles..."
 copy_dotfiles
-echo ""
-
-echo "Installing mintty themes.."
-copy_mintty_themes
 echo ""
 
 echo "Installing bash-git-prompt..."
